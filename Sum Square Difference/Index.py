92c34d4b47dd1124cad21ec7fa273356e07c1f4a
MUHAMMAD SAIFUDIN BIN MOHD JAMAL <145031320+Iamatto01@users.noreply.github.com>
2025-02-24 22:13:38 +0800

    Add files via upload

---
+++ Sum Square Difference/Index.py
@@ -0,0 +1,22 @@
+
+sumsquare = 0
+squaresum = 0
+
+n = int(input("Enter n:"))
+
+for i in range(1,n+1):
+    sumsquare += i**2
+    
+  
+    
+    
+
+for j in range(1,n+1 ):
+    squaresum += j
+    
+squaresum = squaresum**2
+    
+    
+print(sumsquare)     
+print(squaresum)   
+print(squaresum - sumsquare)
