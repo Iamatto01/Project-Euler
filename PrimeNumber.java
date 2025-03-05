07bacf15f7f464c4535d6d9d771c21ab9c07a5e8
MUHAMMAD SAIFUDIN BIN MOHD JAMAL <145031320+Iamatto01@users.noreply.github.com>
2025-03-05 16:11:11 +0800

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,8 @@
+public class PrimeNumber{
+  public static void main(String[] args){
+
+    System.out.println("Enter your desired prime number :");
+    Scanner Scanner = new Scanner(System.in);
+    int Max = Scanner.nextInt();
+  } 
+}
