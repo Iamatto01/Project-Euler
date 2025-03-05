c4d1d04acc67f7dd9e53e84a286521e0b41494d1
MUHAMMAD SAIFUDIN BIN MOHD JAMAL <145031320+Iamatto01@users.noreply.github.com>
2025-03-05 18:11:45 +0800

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,8 +1,48 @@
-public class PrimeNumber{
-  public static void main(String[] args){
+import java.util.*;
 
-    System.out.println("Enter your desired prime number :");
-    Scanner Scanner = new Scanner(System.in);
-    int Max = Scanner.nextInt();
-  } 
-}
+public class PrimeNumber{
+    public static void main(String []args){
+        
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter your desired prime number : ");
+        int Max = Scanner.nextInt();
+        int Prime = PrimeCalc(Max);
+        System.out.println(Prime);
+        Scanner.close();
+
+    
+        }
+        public static int PrimeCalc(int Max){
+            int Prime = 2;
+            int temp = 0;
+            int i = 1;
+            if (Max==1) {return Prime;}
+            else{
+            do{
+                Prime++;
+                int j  = Prime ;
+
+                do{
+                    j--;
+                    if (j==1) {
+                         i++;
+                        break;
+                    }
+                    else{
+                   temp = Prime%j;
+                   if(temp == 0){
+                    Prime ++;
+                    j = Prime;
+                   }}
+                }while(true);
+               // System.out.println("Prime Number " + i + " is " + Prime);
+            }while(i<Max);
+        }
+
+
+            return Prime;
+        }
+      }
+
+        
+    
