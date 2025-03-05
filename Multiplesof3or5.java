912a74a4de4c52c2aef110c562cd53991c47b3e0
MUHAMMAD SAIFUDIN BIN MOHD JAMAL <145031320+Iamatto01@users.noreply.github.com>
2025-03-05 13:25:10 +0800

    Add files via upload

--- Multiplesof3or5.java
+++ Multiplesof3or5.java
@@ -3,40 +3,37 @@ import java.util.*;
 public class Multiplesof3or5 {
 public static void main (String[] args){
         
-System.out.println("Enter Max Number :");
-Scanner Scanner = new Scanner(System.in);
-int Max = Scanner.nextInt();
-Scanner.close();
-
-int Sum = Multiple(Max);
-System.out.println(Sum);
-
+    System.out.println("Enter Max Number :");
+    Scanner Scanner = new Scanner(System.in);
+    int Max = Scanner.nextInt();
+    Scanner.close();
+    
+    int Sum = Multiple(Max);
+    System.out.println(Sum);
+    
 }
 
 public static int Multiple(int Max){
+    int Sum = 0;
+Map<Integer,Integer> Map = new HashMap<>();
+int Sum3 = 0;
+int Sum5 = 0;
+for (int i=1;Sum3 < Max ; i++){
+    Sum3 = 3*i;
+    if(Sum3 >= Max) break;
+    Map.put(Sum3,Sum3);
 
-int Sum = 0;
-int num3 = 0;
-int num5 = 0;
-int i = 1; 
+}
+for (int i=1;Sum5 < Max ; i++){
+    Sum5 = 5*i;
+    if(Sum5 >= Max) break;
+    Map.put(Sum5,Sum5);
 
-do{
-    int temp3 = 3;
-    num3 = temp3 * i;
-    if (num3 >= Max) break;
-    i++;
-    Sum += num3;
-    }while(1 == 1) ;
-    i = 1;
+}
 
-do{
-    int temp5 = 5;
-    num5 = temp5 * i;
-    if (num5 >= Max) break;
-    i++;
-    Sum += num5;
-    }while(1 == 1) ;
-
+for (int value : Map.values()){
+    Sum += value;
+}
     return Sum;
 }
 
